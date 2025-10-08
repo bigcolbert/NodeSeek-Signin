69432a2c184cba7d54c2bd4f356ef60f930217d2
bigcolbert <93502079+bigcolbert@users.noreply.github.com>
2025-10-08 16:39:06 +0800

    Update notify.py

--- notify.py
+++ notify.py
@@ -91,6 +91,7 @@ push_config = {
     'TG_BOT_TOKEN': '',                 # tg 机器人的 TG_BOT_TOKEN，例：1407203283:AAG9rt-6RDaaX0HBLZQq0laNOh898iFYaRQ
     'TG_USER_ID': '',                   # tg 机器人的 TG_USER_ID，例：1434078534
     'TG_THREAD_ID': '',                 # tg 机器人的 TG_THREAD_ID 超级群组话题id，例：2
+    'TG_CHAT_ID': '',                   # tg 机器人的 TG_CHAT_ID 群组id，例：-2
     'TG_API_HOST': '',                  # tg 代理 api
     'TG_PROXY_AUTH': '',                # tg 代理认证参数
     'TG_PROXY_HOST': '',                # tg 机器人的 TG_PROXY_HOST
